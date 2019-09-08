ba7141c55bcb23c7dfc9bbdffcc0dd3d64058150
MohammedJouhar <54748263+MohammedJouhar@users.noreply.github.com>
2019-09-08 17:17:23 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,11 @@
+# this program says helo asks for my name.
+
+print('Hello world')
+print('What is your name?') # ask for thier name
+myname = input()
+print('It is good to meet you', myname)
+print('The length of your name is: ')
+print(len(myname))
+print('Whats is your age?') # ask for thier age
+myage = input()
+print('You will be ' +  str(int(myage)+1) + 'in a year.')
